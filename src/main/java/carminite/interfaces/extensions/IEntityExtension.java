92577021645f767c96c331382c0727f1b6bd2fc6
a6tumn <shinigami7x@proton.me>
2026-08-27 14:33:52 -0400

    Cleaned up implementation.

--- src/main/java/carminite/interfaces/extensions/IEntityExtension.java
+++ src/main/java/carminite/interfaces/extensions/IEntityExtension.java
@@ -1,12 +1,5 @@
 package carminite.interfaces.extensions;
 
-import net.minecraft.world.entity.Entity;
-
-/**
- * Recreates NeoForge's {@code IEntityExtension.canRiderInteract} on Fabric,
- * injected onto {@link Entity} via the classtweaker's
- * {@code transitive-inject-interface}.
- */
 public interface IEntityExtension {
 	default boolean canRiderInteract() {
 		return false;
